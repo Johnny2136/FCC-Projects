b870b73845f3a3208bdbbf7b997c8430ade11964
Johnny Johnson <johnny2136@gmail.com>
2018-08-04 23:17:06 -0400

    Update ObjectOrientedProgramming.js

--- FCC_Challenges/ObjectOrientedProgramming.js
+++ FCC_Challenges/ObjectOrientedProgramming.js
@@ -1,1 +1,8 @@
-
+//Create a Basic JavaScript Object
+//Create a dog object with name and numLegs properties, and set them to a string and a number, respectively.
+let dog = {
+ name: "Alfie",
+ numLegs: 4 
+};
+console.log(dog);
+console.log("--------------------------------------------")
