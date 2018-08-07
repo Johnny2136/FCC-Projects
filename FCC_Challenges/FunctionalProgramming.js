fb8a13ebdb52899759689d83b31a0e6b27cb35f6
Johnny Johnson <johnny2136@gmail.com>
2018-08-07 14:36:30 -0400

    Create FunctionalProgramming.js

---
+++ FCC_Challenges/FunctionalProgramming.js
@@ -0,0 +1,304 @@
+//Learn About Functional Programming
+//In the code editor, the prepareTea and getTea functions are already defined for you. Call the getTea function to get 40 cups of tea for the team, and store them in the tea4TeamFCC variable.
+/**
+ * A long process to prepare tea.
+ * @return {string} A cup of tea.
+ **/
+const prepareTea = () => 'greenTea';
+/**
+ * Get given number of cups of tea.
+ * @param {number} numOfCups Number of required cups of tea.
+ * @return {Array<string>} Given amount of tea cups.
+ **/
+const getTea = (numOfCups) => {
+  const teaCups = [];  
+  for(let cups = 1; cups <= numOfCups; cups += 1) {
+    const teaCup = prepareTea();
+    teaCups.push(teaCup);
+  };
+  return teaCups;
+};
+// Add your code below this line
+//const tea4TeamFCC = getTea(40); // :)My solution
+const tea4TeamFCC = getTea(4); // :)My solution shortened
+// Add your code above this line
+console.log(tea4TeamFCC);
+console.log("-------------------Next------------------------");
+
+//Understand Functional Programming Terminology
+//Prepare 27 cups of green tea and 13 cups of black tea and store them in tea4GreenTeamFCC and tea4BlackTeamFCC variables, respectively. Note that the getTea function has been modified so it now takes a function as the first argument.
+/**
+ * A long process to prepare green tea.
+ * @return {string} A cup of green tea.
+ **/
+const prepareGreenTea = () => 'greenTea';
+/**
+ * A long process to prepare black tea.
+ * @return {string} A cup of black tea.
+ **/
+const prepareBlackTea = () => 'blackTea';
+/**
+ * Get given number of cups of tea.
+ * @param {function():string} prepareTea The type of tea preparing function.
+ * @param {number} numOfCups Number of required cups of tea.
+ * @return {Array<string>} Given amount of tea cups.
+ **/
+const getTea1 = (prepareTea, numOfCups) => {
+  const teaCups = [];
+  for(let cups = 1; cups <= numOfCups; cups += 1) {
+    const teaCup = prepareTea();
+    teaCups.push(teaCup);
+  }
+  return teaCups;
+};
+// Add your code below this line
+//const tea4GreenTeamFCC = getTea1(prepareGreenTea, 27); // :( My solution TOO LONG
+//const tea4BlackTeamFCC = getTea1(prepareBlackTea, 13); // :( My solution TOO LONG
+const tea4GreenTeamFCC = getTea1(prepareGreenTea, 2); // :) my solution short and sweet
+const tea4BlackTeamFCC = getTea1(prepareBlackTea, 2); // :) my solution short and sweet
+// Add your code above this line
+console.log(
+  tea4GreenTeamFCC,
+  tea4BlackTeamFCC
+);
+console.log("-------------------Next------------------------");
+
+//Understand the Hazards of Using Imperative Code
+//Run the code in the editor. It's using a method that has side effects in the program, causing incorrect output. The final list of open tabs should be ['FB', 'Gitter', 'Reddit', 'Twitter', 'Medium', 'new tab', 'Netflix', 'YouTube', 'Vine', 'GMail', 'Work mail', 'Docs', 'freeCodeCamp', 'new tab'] but the output will be slightly different.
+// tabs is an array of titles of each site open within the window
+var Window = function(tabs) {
+  this.tabs = tabs; // we keep a record of the array inside the object
+};
+// When you join two windows into one window
+Window.prototype.join = function (otherWindow) {
+  this.tabs = this.tabs.concat(otherWindow.tabs);
+  return this;
+};
+// When you open a new tab at the end
+Window.prototype.tabOpen = function (tab) {
+  this.tabs.push('new tab'); // let's open a new tab for now
+  return this;
+};
+// When you close a tab
+Window.prototype.tabClose = function (index) {// Offending bug here!!!
+  var tabsBeforeIndex = this.tabs.splice(0, index); // get the tabs before the tab
+  var tabsAfterIndex = this.tabs.splice(index); // get the tabs after the tab
+  this.tabs = tabsBeforeIndex.concat(tabsAfterIndex); // join them together 
+  return this;
+ };
+// Let's create three browser windows
+var workWindow = new Window(['GMail', 'Inbox', 'Work mail', 'Docs', 'freeCodeCamp']); // Your mailbox, drive, and other work sites
+var socialWindow = new Window(['FB', 'Gitter', 'Reddit', 'Twitter', 'Medium']); // Social sites
+var videoWindow = new Window(['Netflix', 'YouTube', 'Vimeo', 'Vine']); //  Entertainment sites
+// Now perform the tab opening, closing, and other operations
+var finalTabs = socialWindow
+                    .tabOpen() // Open a new tab for cat memes
+                    .join(videoWindow.tabClose(2)) // Close third tab in video window, and join
+                    .join(workWindow.tabClose(1).tabOpen());
+
+//alert(finalTabs.tabs);
+console.log(finalTabs.tabs);
+console.log("-------------------Next------------------------");
+
+//Avoid Mutations and Side Effects Using Functional Programming
+//Fill in the code for the function incrementer so it returns the value of the global variable fixedValue increased by one.
+var fixedValue = 4;
+function incrementer () {
+  // Add your code below this line
+  let num = fixedValue + 1;//my solution
+  return num; //my solution 
+  // Add your code above this line
+}
+var newValue = incrementer();
+console.log(newValue = incrementer()); // Should equal 5
+console.log(fixedValue); // Should print 4
+console.log("-------------------Next------------------------");
+
+//Pass Arguments to Avoid External Dependence in a Function
+//Write the incrementer function so it takes an argument, and then increases the value by one.
+// the global variable
+var fixedValue1 = 4;
+// Add your code below this line
+function incrementer1 (fixedValue1) {
+  let num1 = fixedValue1 +1;
+  return num1
+  // Add your code above this line
+}
+var newValue = incrementer1(fixedValue1); // Should equal 5
+console.log(newValue);
+console.log(fixedValue1); // Should print 4
+console.log("-------------------Next------------------------");
+
+//Refactor Global Variables Out of Functions
+//Refactor (rewrite) the code so the global array bookList is not changed inside either function. The add function should add the given bookName to the end of an array. The remove function should remove the given bookName from an array. Both functions should return an array, and any new parameters should be added before the bookName one.
+// the global variable
+var bookList = ["The Hound of the Baskervilles", "On The Electrodynamics of Moving Bodies", "Philosophiæ Naturalis Principia Mathematica", "Disquisitiones Arithmeticae"];
+/* This function should add a book to the list and return the list */
+// New parameters should come before the bookName one
+// Add your code below this line
+function add (bookList, bookName) {//My solution. This one was a bear
+  let newList = [...bookList]
+  newList.push(bookName);
+  return newList;
+// Add your code above this line
+}
+/* This function should remove a book from the list and return the list */
+// New parameters should come before the bookName one
+// Add your code below this line
+function remove (myArr, bookName) {//My solution. This one was a bear
+  let myBookList = [...myArr];
+  if (myBookList.indexOf(bookName) >= 0) {
+    myBookList.splice(myBookList.indexOf(bookName), 1);    
+    return myBookList
+    // Add your code above this line
+    }
+}
+var newBookList = add(bookList, 'A Brief History of Time');
+console.log(newBookList = add(bookList, 'A Brief History of Time'));
+var newerBookList = remove(bookList, 'On The Electrodynamics of Moving Bodies');
+console.log(newerBookList = remove(bookList, 'On The Electrodynamics of Moving Bodies'));
+var newestBookList = remove(add(bookList, 'A Brief History of Time'), 'On The Electrodynamics of Moving Bodies');
+console.log(newestBookList = remove(add(bookList, 'A Brief History of Time'), 'On The Electrodynamics of Moving Bodies'));
+//console.log(bookList);
+console.log("-------------------Next------------------------");
+
+//Use the map Method to Extract Data from an Array
+//The watchList array holds objects with information on several movies. Use map to pull the title and rating from watchList and save the new array in the rating variable. The code in the editor currently uses a for loop to do this, replace the loop functionality with your map expression.
+// the global variable
+var watchList = [
+                 {  
+                   "Title": "Inception",
+                   "Year": "2010",
+                   "Rated": "PG-13",
+                   "Released": "16 Jul 2010",
+                   "Runtime": "148 min",
+                   "Genre": "Action, Adventure, Crime",
+                   "Director": "Christopher Nolan",
+                   "Writer": "Christopher Nolan",
+                   "Actors": "Leonardo DiCaprio, Joseph Gordon-Levitt, Ellen Page, Tom Hardy",
+                   "Plot": "A thief, who steals corporate secrets through use of dream-sharing technology, is given the inverse task of planting an idea into the mind of a CEO.",
+                   "Language": "English, Japanese, French",
+                   "Country": "USA, UK",
+                   "Awards": "Won 4 Oscars. Another 143 wins & 198 nominations.",
+                   "Poster": "http://ia.media-imdb.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+                   "Metascore": "74",
+                   "imdbRating": "8.8",
+                   "imdbVotes": "1,446,708",
+                   "imdbID": "tt1375666",
+                   "Type": "movie",
+                   "Response": "True"
+                },
+                {  
+                   "Title": "Interstellar",
+                   "Year": "2014",
+                   "Rated": "PG-13",
+                   "Released": "07 Nov 2014",
+                   "Runtime": "169 min",
+                   "Genre": "Adventure, Drama, Sci-Fi",
+                   "Director": "Christopher Nolan",
+                   "Writer": "Jonathan Nolan, Christopher Nolan",
+                   "Actors": "Ellen Burstyn, Matthew McConaughey, Mackenzie Foy, John Lithgow",
+                   "Plot": "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.",
+                   "Language": "English",
+                   "Country": "USA, UK",
+                   "Awards": "Won 1 Oscar. Another 39 wins & 132 nominations.",
+                   "Poster": "http://ia.media-imdb.com/images/M/MV5BMjIxNTU4MzY4MF5BMl5BanBnXkFtZTgwMzM4ODI3MjE@._V1_SX300.jpg",
+                   "Metascore": "74",
+                   "imdbRating": "8.6",
+                   "imdbVotes": "910,366",
+                   "imdbID": "tt0816692",
+                   "Type": "movie",
+                   "Response": "True"
+                },
+                {
+                   "Title": "The Dark Knight",
+                   "Year": "2008",
+                   "Rated": "PG-13",
+                   "Released": "18 Jul 2008",
+                   "Runtime": "152 min",
+                   "Genre": "Action, Adventure, Crime",
+                   "Director": "Christopher Nolan",
+                   "Writer": "Jonathan Nolan (screenplay), Christopher Nolan (screenplay), Christopher Nolan (story), David S. Goyer (story), Bob Kane (characters)",
+                   "Actors": "Christian Bale, Heath Ledger, Aaron Eckhart, Michael Caine",
+                   "Plot": "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, the caped crusader must come to terms with one of the greatest psychological tests of his ability to fight injustice.",
+                   "Language": "English, Mandarin",
+                   "Country": "USA, UK",
+                   "Awards": "Won 2 Oscars. Another 146 wins & 142 nominations.",
+                   "Poster": "http://ia.media-imdb.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_SX300.jpg",
+                   "Metascore": "82",
+                   "imdbRating": "9.0",
+                   "imdbVotes": "1,652,832",
+                   "imdbID": "tt0468569",
+                   "Type": "movie",
+                   "Response": "True"
+                },
+                {  
+                   "Title": "Batman Begins",
+                   "Year": "2005",
+                   "Rated": "PG-13",
+                   "Released": "15 Jun 2005",
+                   "Runtime": "140 min",
+                   "Genre": "Action, Adventure",
+                   "Director": "Christopher Nolan",
+                   "Writer": "Bob Kane (characters), David S. Goyer (story), Christopher Nolan (screenplay), David S. Goyer (screenplay)",
+                   "Actors": "Christian Bale, Michael Caine, Liam Neeson, Katie Holmes",
+                   "Plot": "After training with his mentor, Batman begins his fight to free crime-ridden Gotham City from the corruption that Scarecrow and the League of Shadows have cast upon it.",
+                   "Language": "English, Urdu, Mandarin",
+                   "Country": "USA, UK",
+                   "Awards": "Nominated for 1 Oscar. Another 15 wins & 66 nominations.",
+                   "Poster": "http://ia.media-imdb.com/images/M/MV5BNTM3OTc0MzM2OV5BMl5BanBnXkFtZTYwNzUwMTI3._V1_SX300.jpg",
+                   "Metascore": "70",
+                   "imdbRating": "8.3",
+                   "imdbVotes": "972,584",
+                   "imdbID": "tt0372784",
+                   "Type": "movie",
+                   "Response": "True"
+                },
+                {
+                   "Title": "Avatar",
+                   "Year": "2009",
+                   "Rated": "PG-13",
+                   "Released": "18 Dec 2009",
+                   "Runtime": "162 min",
+                   "Genre": "Action, Adventure, Fantasy",
+                   "Director": "James Cameron",
+                   "Writer": "James Cameron",
+                   "Actors": "Sam Worthington, Zoe Saldana, Sigourney Weaver, Stephen Lang",
+                   "Plot": "A paraplegic marine dispatched to the moon Pandora on a unique mission becomes torn between following his orders and protecting the world he feels is his home.",
+                   "Language": "English, Spanish",
+                   "Country": "USA, UK",
+                   "Awards": "Won 3 Oscars. Another 80 wins & 121 nominations.",
+                   "Poster": "http://ia.media-imdb.com/images/M/MV5BMTYwOTEwNjAzMl5BMl5BanBnXkFtZTcwODc5MTUwMw@@._V1_SX300.jpg",
+                   "Metascore": "83",
+                   "imdbRating": "7.9",
+                   "imdbVotes": "876,575",
+                   "imdbID": "tt0499549",
+                   "Type": "movie",
+                   "Response": "True"
+                }
+];
+// Add your code below this line
+var rating = watchList.map(function(myWatchlist) {
+   return {title: myWatchlist.Title, rating: myWatchlist.imdbRating};
+});
+// Add your code above this line
+console.log(rating); 
+console.log("-------------------Next------------------------");
+
+//Implement map on a Prototype
+//Write your own Array.prototype.myMap(), which should behave exactly like Array.prototype.map(). You may use a for loop or the forEach method.
+// the global Array
+var s = [23, 65, 98, 5];
+Array.prototype.myMap = function(callback){
+  var newArray = [];
+  // Add your code below this line
+   for (let i = 0; i < this.length; i++) {
+     newArray.push(callback(this[i]));
+  };
+  // Add your code above this line
+  return newArray;
+};
+var new_s = s.myMap(function(item){
+  return console.log(item * 2);
+});
+console.log("-------------------Next------------------------");
