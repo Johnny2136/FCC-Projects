07e05e70fb10d9d84977f7ed3d6938bff11eb743
Johnny Johnson <johnny2136@gmail.com>
2018-08-08 15:27:55 -0400

    Update FunctionalProgramming.js

--- FCC_Challenges/FunctionalProgramming.js
+++ FCC_Challenges/FunctionalProgramming.js
@@ -285,6 +285,7 @@ var rating = watchList.map(function(myWatchlist) {
 console.log(rating); 
 console.log("-------------------Next------------------------");
 
+
 //Implement map on a Prototype
 //Write your own Array.prototype.myMap(), which should behave exactly like Array.prototype.map(). You may use a for loop or the forEach method.
 // the global Array
@@ -292,13 +293,190 @@ var s = [23, 65, 98, 5];
 Array.prototype.myMap = function(callback){
   var newArray = [];
   // Add your code below this line
-   for (let i = 0; i < this.length; i++) {
-     newArray.push(callback(this[i]));
-  };
-  // Add your code above this line
+  console.log("this -->" + this.length);//Debug
+   for (let i = 0; i < this.length; i++) {     
+    newArray.push(callback(this[i]));
+   };
+   //Either of these solutions will work I tried both "for" and "forEach" below;
+  console.log("this -->" + this);//Debug  
+  this.forEach(function(item){
+    newArray.push(callback(item));
+  });
+    // Add your code above this line
   return newArray;
 };
 var new_s = s.myMap(function(item){
   return console.log(item * 2);
 });
+console.log("s -->" + s);//Debug
+// Resourses for this excersize: [Mozzelia.org](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_objectcts/Array/forEach#Converting_a_for_loop_to_forEach)
 console.log("-------------------Next------------------------");
+
+//Use the filter Method to Extract Data from an Array
+//The variable watchList holds an array of objectcts with information on several movies. Use a combination of filter and map to return a new array of objectcts with only title and rating keys, but where imdbRating is greater than or equal to 8.0. Note that the rating values are saved as strings in the objectct and you may want to convert them into numbers to perform mathematical operations on them.
+// the global variable
+var watchList = [
+                 {  
+                   "Title": "Inception",
+                   "Year": "2010",
+                   "Rated": "PG-13",
+                   "Released": "16 Jul 2010",
+                   "Runtime": "148 min",
+                   "Genre": "Action, Adventure, Crime",
+                   "Director": "Christopher Nolan",
+                   "Writer": "Christopher Nolan",
+                   "Actors": "Leonardo DiCaprio, Joseph Gordon-Levitt, Ellen Page, Tom Hardy",
+                   "Plot": "A thief, who steals corporate secrets through use of dream-sharing technology, is given the inverse task of planting an idea into the mind of a CEO.",
+                   "Language": "English, Japanese, French",
+                   "Country": "USA, UK",
+                   "Awards": "Won 4 Oscars. Another 143 wins & 198 nominations.",
+                   "Poster": "http://ia.media-imdb.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+                   "Metascore": "74",
+                   "imdbRating": "8.8",
+                   "imdbVotes": "1,446,708",
+                   "imdbID": "tt1375666",
+                   "Type": "movie",
+                   "Response": "True"
+                },
+                {  
+                   "Title": "Interstellar",
+                   "Year": "2014",
+                   "Rated": "PG-13",
+                   "Released": "07 Nov 2014",
+                   "Runtime": "169 min",
+                   "Genre": "Adventure, Drama, Sci-Fi",
+                   "Director": "Christopher Nolan",
+                   "Writer": "Jonathan Nolan, Christopher Nolan",
+                   "Actors": "Ellen Burstyn, Matthew McConaughey, Mackenzie Foy, John Lithgow",
+                   "Plot": "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.",
+                   "Language": "English",
+                   "Country": "USA, UK",
+                   "Awards": "Won 1 Oscar. Another 39 wins & 132 nominations.",
+                   "Poster": "http://ia.media-imdb.com/images/M/MV5BMjIxNTU4MzY4MF5BMl5BanBnXkFtZTgwMzM4ODI3MjE@._V1_SX300.jpg",
+                   "Metascore": "74",
+                   "imdbRating": "8.6",
+                   "imdbVotes": "910,366",
+                   "imdbID": "tt0816692",
+                   "Type": "movie",
+                   "Response": "True"
+                },
+                {
+                   "Title": "The Dark Knight",
+                   "Year": "2008",
+                   "Rated": "PG-13",
+                   "Released": "18 Jul 2008",
+                   "Runtime": "152 min",
+                   "Genre": "Action, Adventure, Crime",
+                   "Director": "Christopher Nolan",
+                   "Writer": "Jonathan Nolan (screenplay), Christopher Nolan (screenplay), Christopher Nolan (story), David S. Goyer (story), Bob Kane (characters)",
+                   "Actors": "Christian Bale, Heath Ledger, Aaron Eckhart, Michael Caine",
+                   "Plot": "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, the caped crusader must come to terms with one of the greatest psychological tests of his ability to fight injustice.",
+                   "Language": "English, Mandarin",
+                   "Country": "USA, UK",
+                   "Awards": "Won 2 Oscars. Another 146 wins & 142 nominations.",
+                   "Poster": "http://ia.media-imdb.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_SX300.jpg",
+                   "Metascore": "82",
+                   "imdbRating": "9.0",
+                   "imdbVotes": "1,652,832",
+                   "imdbID": "tt0468569",
+                   "Type": "movie",
+                   "Response": "True"
+                },
+                {  
+                   "Title": "Batman Begins",
+                   "Year": "2005",
+                   "Rated": "PG-13",
+                   "Released": "15 Jun 2005",
+                   "Runtime": "140 min",
+                   "Genre": "Action, Adventure",
+                   "Director": "Christopher Nolan",
+                   "Writer": "Bob Kane (characters), David S. Goyer (story), Christopher Nolan (screenplay), David S. Goyer (screenplay)",
+                   "Actors": "Christian Bale, Michael Caine, Liam Neeson, Katie Holmes",
+                   "Plot": "After training with his mentor, Batman begins his fight to free crime-ridden Gotham City from the corruption that Scarecrow and the League of Shadows have cast upon it.",
+                   "Language": "English, Urdu, Mandarin",
+                   "Country": "USA, UK",
+                   "Awards": "Nominated for 1 Oscar. Another 15 wins & 66 nominations.",
+                   "Poster": "http://ia.media-imdb.com/images/M/MV5BNTM3OTc0MzM2OV5BMl5BanBnXkFtZTYwNzUwMTI3._V1_SX300.jpg",
+                   "Metascore": "70",
+                   "imdbRating": "8.3",
+                   "imdbVotes": "972,584",
+                   "imdbID": "tt0372784",
+                   "Type": "movie",
+                   "Response": "True"
+                },
+                {
+                   "Title": "Avatar",
+                   "Year": "2009",
+                   "Rated": "PG-13",
+                   "Released": "18 Dec 2009",
+                   "Runtime": "162 min",
+                   "Genre": "Action, Adventure, Fantasy",
+                   "Director": "James Cameron",
+                   "Writer": "James Cameron",
+                   "Actors": "Sam Worthington, Zoe Saldana, Sigourney Weaver, Stephen Lang",
+                   "Plot": "A paraplegic marine dispatched to the moon Pandora on a unique mission becomes torn between following his orders and protecting the world he feels is his home.",
+                   "Language": "English, Spanish",
+                   "Country": "USA, UK",
+                   "Awards": "Won 3 Oscars. Another 80 wins & 121 nominations.",
+                   "Poster": "http://ia.media-imdb.com/images/M/MV5BMTYwOTEwNjAzMl5BMl5BanBnXkFtZTcwODc5MTUwMw@@._V1_SX300.jpg",
+                   "Metascore": "83",
+                   "imdbRating": "7.9",
+                   "imdbVotes": "876,575",
+                   "imdbID": "tt0499549",
+                   "Type": "movie",
+                   "Response": "True"
+                }
+];
+// Add your code below this line
+var filteredList = watchList.filter(obj => Number(obj.imdbRating) >= 8.0) .map((obj => ({title: obj.Title, rating: obj.imdbRating})));
+// Add your code above this line
+console.log(filteredList);
+// Resourses for this excersize: [Array Map, Filter and Reduce in JS](https://atendesigngroup.com/blog/array-map-filter-and-reduce-js)
+console.log("-------------------Next------------------------"); 
+
+//Implement the filter Method on a Prototype
+//Write your own Array.prototype.myFilter(), which should behave exactly like Array.prototype.filter(). You may use a for loop or the Array.prototype.forEach() method.
+// the global Array
+var s = [23, 65, 98, 5];
+Array.prototype.myFilter = function(callback){
+  var newArray = [];
+  // Add your code below this line
+   console.log("this -->" + s);//Debug  
+   this.forEach(function(obj) { //My solution
+    console.log("this -->" + obj);//Debug  
+     if (callback(obj) == true) { //My solution
+       newArray.push(obj); } });//My solution
+      console.log("this -->" + newArray);//Debug  
+  // Add your code above this line
+  return newArray;
+};
+var new_s = s.myFilter(function(item){
+  return item % 2 === 1;
+});
+// Resourses for this excersize: [Array Map, Filter and Reduce in JS](https://atendesigngroup.com/blog/array-map-filter-and-reduce-js)
+console.log("-------------------Next------------------------");
+
+//Return Part of an Array Using the slice Method
+//Use the slice method in the sliceArray function to return part of the anim array given the provided beginSlice and endSlice indices. The function should return an array.
+function sliceArray(anim, beginSlice, endSlice) {
+  // Add your code below this line
+  var newArray = anim.slice(beginSlice, endSlice);
+  return newArray;
+  // Add your code above this line
+}
+var inputAnim = ["Cat", "Dog", "Tiger", "Zebra", "Ant"];
+console.log(sliceArray(inputAnim, 1, 3));
+console.log("-------------------Next------------------------");
+
+//Remove Elements from an Array Using slice Instead of splice
+//Rewrite the function nonMutatingSplice by using slice instead of splice. It should limit the provided cities array to a length of 3, and return a new array with only the first three items.
+//Do not mutate the original array provided to the function.
+function nonMutatingSplice(cities) {
+  // Add your code below this line
+  return cities.splice(3);
+  
+  // Add your code above this line
+}
+var inputCities = ["Chicago", "Delhi", "Islamabad", "London", "Berlin"];
+nonMutatingSplice(inputCities);
+
