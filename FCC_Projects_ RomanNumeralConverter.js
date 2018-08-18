a2a423830c7d3f44f83d64c1e6470e8dd0d6049e
Johnny Johnson <johnny2136@gmail.com>
2018-08-18 15:51:23 -0400

    Delete FCC_Projects_ RomanNumeralConverter.js

--- FCC_Projects_ RomanNumeralConverter.js
+++
@@ -1,7 +0,0 @@
-console.log("Roman Numeral Converter")
-//Convert the given number into a roman numeral. All roman numerals answers should be provided in upper-case.
-function convertToRoman(num) {
- return num;
-}
-
-convertToRoman(36);
