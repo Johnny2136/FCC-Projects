c26fa44cbf8464f877bf5aef13d3642bc0d537fe
Johnny Johnson <johnny2136@gmail.com>
2018-08-18 17:07:29 -0400

    Working on second project

--- FCC_JS_Projects/RomanNumeralConverter.js
+++ FCC_JS_Projects/RomanNumeralConverter.js
@@ -1,19 +1,26 @@
+console.log("Roman Numeral Converter")
+//Convert the given number into a roman numeral. All roman numerals answers should be provided in upper-case.
+//https://repl.it/@JohnJohnson2/FCCProjects-RomanNumeralConverterjs
 /***************************************************************
  * JavaScript Algorithms and Data Structures Projects:         * 
- * Roman Numeral Converter, JJ 2018                                 *
- * https://jsfiddle.net/johnny2136/8m6937xd/20/                *
+ * Roman Numeral Converter, JJ 2018                            *
+ * https://jsfiddle.net/johnny2136/7b2qwepo/21/                *
  ***************************************************************/
-
 function convertToRoman(num) {
-   
-  var rom = '';
-  var decimal = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
-  var roman = ["M", "CM","D","CD","C", "XC", "L", "XL", "X","IX","V","IV","I"];
-  for (var i = 0;i<=decimal.length;i++) {
-    while (num%decimal[i] < num) {     
-      rom += roman[i];
-      num -= decimal[i];
-    }
-  }
+   //Make 2 arrays.
+  const decArr = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+  const romArr = ["M", "CM","D","CD","C", "XC", "L", "XL", "X","IX","V","IV","I"];
+  //Variable for solution.
+  let rom = " ";
+  //While loop inside a for to convert the Number to Roman number
+  console.log(num);
+  for (let i = 0; i <= decArr.length; i++) {
+    while (num%decArr[i] < num) {     
+      rom += romArr[i];
+      num -= decArr[i];
+    };    
+  }; 
+  console.log("converts to " + rom);
   return rom;
-convertToRoman(4);
+};
+convertToRoman(36);
