71c0e24dfc3c83a36ce9e5ada444f0df06be3b8f
Johnny Johnson <johnny2136@gmail.com>
2018-08-20 18:59:22 -0400

    Completed day 27 :Alien:

--- FCC_JS_Projects/caesarsCipher.js
+++ FCC_JS_Projects/caesarsCipher.js
@@ -1,9 +1,27 @@
+/***************************************************************
+ * JavaScript Algorithms and Data Structures Projects:         * 
+ * CaesarsCipher, JJ 2018                                      *
+ * https://repl.it/@JohnJohnson2/FCCProjectCaesarsCipher       *
+ ***************************************************************/
+//My solution
 function rot13(str) { // LBH QVQ VG!
+  let myStr = "";
+  console.log(str + "");//Debugging str contents
+  console.log((str + "").replace(/[A-Z]/gi, function (myStr) {
+    return String.fromCharCode(myStr.charCodeAt(0) + (myStr.toLowerCase() < "n" ? 13 : -13));
+  }));//Debugging conversion routine, should decode in ROT 13 or encode if plain text
+  return (str + "").replace(/[A-Z]/gi, function (myStr) {
+    return String.fromCharCode(myStr.charCodeAt(0) + (myStr.toLowerCase() < "n" ? 13 : -13));
+  });
+};
+
+
+//I was first trying this, but wanted to use the "?" operator above.
+function rot13alt(str) { // LBH QVQ VG!
   const a = "A".charCodeAt(0);
   const n = "N".charCodeAt(0);
   const z = "Z".charCodeAt(0);
   let myArr = [];
-
   for(let i =0; i < str.length; i++){
     let code = str.charCodeAt(i);
     if(code >= a && code <= z){
@@ -16,6 +34,5 @@ function rot13(str) { // LBH QVQ VG!
     };
      return myArr.join("");
   };
-
 // Change the inputs below to test
-rot13("SERR PBQR PNZC");
+rot13alt("SERR PBQR PNZC");
